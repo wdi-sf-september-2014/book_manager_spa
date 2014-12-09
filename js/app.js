eb9d5c7c7468cc1da78aeb95d999ca11ab2c688c
Arun Sood <arsood@Aruns-MacBook-Pro.local>
2014-12-08 16:53:01 -0800

    Further work

--- js/app.js
+++ js/app.js
@@ -15,6 +15,64 @@ var newBookTemplate;
 	newBookTemplate = Handlebars.compile(newBookTemplateSource);
 })();
 
+//Set up books collection
+
+var Books = Backbone.Collection.extend({
+	url: "http://daretodiscover.net/books"
+});
+
+var books = new Books();
+
+//Set up book model
+
+var Book = Backbone.Model.extend({
+	urlRoot: "http://daretodiscover.net/books"
+});
+
+//Set up book list view
+
+var BookList = Backbone.View.extend({
+	el: "#container",
+
+	render: function() {
+		var that = this;
+		books.fetch({
+			success: function() {
+				var html = bookListTemplate({
+					allBooks: books.models
+				});
+
+				//$("#container").html(html);
+				that.$el.html(html);
+			}
+		});
+	}
+});
+
+//Set up edit book view
+
+var EditBook = Backbone.View.extend({
+	el: "#container",
+
+	render: function(id) {
+		var that = this;
+		var book = new Book({
+			id: id
+		});
+
+		book.fetch({
+			success: function() {
+				var html = editBookTemplate({
+					bookInfo: book
+				});
+
+				//$("#container").html(html);
+				that.$el.html(html);
+			}
+		});
+	}
+});
+
 //Set up routes
 
 var Router = Backbone.Router.extend({
@@ -29,18 +87,19 @@ var router = new Router();
 
 //Define each route action
 
+//Index route
 router.on("route:index", function() {
-	var html = bookListTemplate();
-
-	$("#container").html(html);
+	var booklist = new BookList();
+	booklist.render();
 });
 
+//Edit book route
 router.on("route:edit_book", function(id) {
-	var html = editBookTemplate();
-
-	$("#container").html(html);
+	var editbook = new EditBook();
+	editbook.render(id);
 });
 
+//Show new book form
 router.on("route:new_book", function() {
 	var html = newBookTemplate();
 
